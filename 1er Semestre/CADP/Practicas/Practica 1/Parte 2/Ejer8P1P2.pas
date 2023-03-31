cdeebe340593cbfa6204296b88a0224784311169
Vicente García Marti <71749981+vicen621@users.noreply.github.com>
2023-03-31 08:27:09 -0300

    Update Ejer8P1P2.pas

--- 1er Semestre/CADP/Practicas/Practica 1/Parte 2/Ejer8P1P2.pas
+++ 1er Semestre/CADP/Practicas/Practica 1/Parte 2/Ejer8P1P2.pas
@@ -2,37 +2,28 @@
 PROGRAM Ejer8P1P2;
 
 VAR
-    day, sells1, sells2, sells3, total, value: integer;    
+    day: integer;
+    sellsDay, total, value: real;    
 
 BEGIN
     total := 0;
 
-    sells1 := 0;
-    sells2 := 0;
-    sells3 := 0;
-
-    for day := 1 to 3 do BEGIN
+    for day := 1 to 31 do BEGIN
         writeln('Indique las ventas para el dia: ', day);
         
         readln(value);
 
-        while (value <> 0) do BEGIN
-            case day of
-                1: sells1 := sells1 + value;
-                2: sells2 := sells2 + value;
-                3: sells3 := sells3 + value;
-            END;
+        sellsDay := 0;
 
+        while (value <> 0) do BEGIN
+            sellsDay := sellsDay + value;
             total := total + value;
 
             readln(value);
         END;
+
+        writeln('Ventas del dia ', day, ': ', sellsDay);
     END;
 
-    writeln('Ventas por dias: ');
-    writeln('Dia 1: ', sells1);
-    writeln('Dia 2: ', sells2);
-    writeln('Dia 3: ', sells3);
-    writeln(' ');
     writeln('Ventas totales: ', total);
 END.
