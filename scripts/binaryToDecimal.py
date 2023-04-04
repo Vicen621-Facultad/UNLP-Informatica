e6fe382c2340f86780eae7b9d07d6c4231cba705
Vicen621 <71749981+vicen621@users.noreply.github.com>
2023-04-04 12:03:36 -0300

    Macbook-Vicen vault backup: 2023-04-04 12:03:36

--- scripts/binaryToDecimal.py
+++ scripts/binaryToDecimal.py
@@ -1,12 +1,14 @@
 loop = True
 
-def dec_to_bss(binary):
+
+def bss_to_dec(binary):
     i = len(binary) - 1
     dec = 0
     for x in binary:
         dec += int(x)*(2**i)
         i -= 1
     return dec
+
 
 systems = {
     'bss': 'Binario Sin Signo',
@@ -21,38 +23,41 @@ while loop:
     mode = input(
         'Ingresa el sistema de representacion, posibles: bss, bcs, ca1, ca2 y ex2: ').lower()
     decimal = 0
-    binary_print = binary;
+    binary_print = binary
 
     sign = 1 if (binary[0] == '0') else -1
 
     match mode:
         case 'bss':
-            decimal = dec_to_bss(binary)
+            decimal = bss_to_dec(binary)
 
         case 'bcs':
             binary = binary[1:]
-            decimal = dec_to_bss(binary)
+            decimal = bss_to_dec(binary)
             decimal *= sign
 
         case 'ca1':
             binary = binary[1:]
 
             ca1_bin = ''.join('1' if c == '0' else '0' for c in binary)
-            decimal = dec_to_bss(ca1_bin)
+            decimal = bss_to_dec(ca1_bin)
             decimal *= sign
 
         case 'ca2':
             partition = binary[::-1].partition('1')
             flip = ''.join('1' if c == '0' else '0' for c in partition[2])
             ca2 = f'{partition[0]}{partition[1]}{flip}'
-            decimal = dec_to_bss(ca2[::-1] if sign == -1 else binary) * sign
-        
+            decimal = bss_to_dec(ca2[::-1] if sign == -1 else binary) * sign
+
         case 'ex2':
-            print()
-        
+            EXCESS = 2**(len(binary)-1)
+            print(EXCESS)
+            decimal = bss_to_dec(binary) - EXCESS
+
         case other:
             print('Sistema no reconocido, por favor ingrese un sistema valido')
             continue
 
-    print(f'\n{binary_print}₂ interpretado en {systems[mode]} es: {decimal}₁₀ \n')
+    print(
+        f'\n{binary_print}₂ interpretado en {systems[mode]} es: {decimal}₁₀ \n')
     loop = input('Queres ingresar otro numero? \ns/n: ').lower() == 's'
