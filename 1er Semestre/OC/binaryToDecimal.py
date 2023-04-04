008c0690b9b8a304d66b8979fe6014851b5f2dbf
Vicen621 <71749981+vicen621@users.noreply.github.com>
2023-04-04 09:53:36 -0300

    Macbook-Vicen vault backup: 2023-04-04 09:53:36

--- 1er Semestre/OC/binaryToDecimal.py
+++ 1er Semestre/OC/binaryToDecimal.py
@@ -1,5 +1,10 @@
-binary = input('Ingresa el numero en binario')
+binary = float(input('Ingresa el numero en binario'))
 mode = input('Ingresa el sistema de representacion, posibles: bss, bcs, ca1, ca2 y ex2')
 decimal = 0;
 i = 0;
 
+match mode:
+    case 'bss':
+        while binary > 0:
+            decimal += (binary % 10)**i;
+            ++i
