01e025065e6432d3165e353fe639ff793f997219
Vicente García Marti <71749981+vicen621@users.noreply.github.com>
2023-04-06 13:01:18 -0300

    Update Ejer6P2P1.pas

--- 1er Semestre/CADP/Practicas/Practica 2/Parte 1/Ejer6P2P1.pas
+++ 1er Semestre/CADP/Practicas/Practica 2/Parte 1/Ejer6P2P1.pas
@@ -6,6 +6,7 @@ procedure numeroParMasAlto;
     begin
         max := -1;
 
+        readln(value);
         while(value >= 0) begin
             readln(value);
             if (value MOD 2 = 0) and (value > max) then
