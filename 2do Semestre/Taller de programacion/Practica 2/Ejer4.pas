c4b6dc20e650a3621b96bc2b0190a932dd8bd8ea
Vicen621 <71749981+vicen621@users.noreply.github.com>
2023-08-31 10:52:16 -0300

    Terminado ejercicio 4, empezado ejercicio 5

--- 2do Semestre/Taller de programacion/Practica 2/Ejer4.pas
+++ 2do Semestre/Taller de programacion/Practica 2/Ejer4.pas
@@ -1,10 +1,12 @@
 program ejer4;
+const
+	DimF = 20;
 type
-	vector = array[1..20] of integer;
+	vector = array[1..DimF] of integer;
 	
 procedure agregarVector(num: integer; var v: vector; var dimL: integer);
 begin
-	if (dimL + 1 <= 20) then begin
+	if (dimL + 1 <= DimF) then begin
 		dimL := dimL + 1;
 		v[dimL] := num;
 	end;
@@ -21,37 +23,66 @@ begin
 	end;
 end;
 
-procedure maximoVector(v: vector; dimL: integer): integer;
+procedure imprimirVector(v: vector; dimL: integer);
+var
+    i: integer;
+begin
+    for i := 1 to dimL do
+        writeln(i, ': ', v[i]);
+end;
+
+function maximoVector(v: vector; dimL: integer): integer;
 var
 	max: integer;
 begin
 	if (dimL = 0) then
-		maximoLista := 0
+		maximoVector := 0
 	else begin
-		max := maximoLista(dimL - 1);
+		max := maximoVector(v, dimL - 1);
 		if (v[dimL] > max) then
 			max := v[dimL];
 			
-		maximoLista := max;
+		maximoVector := max;
 	end;
 end;
 	
-procedure minimoVector(v: vector; dimL: integer): integer;
+function minimoVector(v: vector; dimL: integer): integer;
 var
 	min: integer;
 begin
 	if (dimL = 0) then
-		minimoVector := 0
+		minimoVector := 100
 	else begin
-		min := minimoVector(dimL - 1);
-		if (v[dimL] > max) then
+		min := minimoVector(v, dimL - 1);
+		if (v[dimL] < min) then
 			min := v[dimL];
 			
 		minimoVector := min;
 	end;
 end;
+
+function sumarVector(v: vector; dimL: integer): integer;
+var
+	total: integer;
+begin
+	if (dimL = 0) then
+		total := 0
+	else
+		total := v[dimL] + sumarVector(v, dimL - 1);
 	
+	sumarVector := total;
+end;
+
+var
+	v: vector;
+	dimL: integer;
+
 begin
 	Randomize;
 	generarVector(v, dimL);
+	writeln('------- VECTOR ------- ');
+	imprimirVector(v, dimL);
+	writeln('Minimo del vector: ', minimoVector(v, dimL));
+	writeln('Maximo del vector: ', maximoVector(v, dimL));
+	writeln('Suma de los valores: ', sumarVector(v, dimL))
 end.
---
+++ 2do Semestre/Taller de programacion/Practica 2/Ejer5.pas
@@ -0,0 +1,67 @@
+program ejer5;
+const
+    DIMF = 20;
+type
+    indice = -1..DIMF;
+    vector = array[1..DIMF] of integer;
+
+procedure agregarVector(num: integer; var v: vector; var dimL: integer);
+begin
+	if (dimL + 1 <= DIMF) then begin
+		dimL := dimL + 1;
+		v[dimL] := num;
+	end;
+end;
+
+procedure imprimirVector(v: vector; dimL: integer);
+var
+    i: integer;
+begin
+    for i := 1 to dimL do
+        writeln(i, ': ', v[i]);
+end;
+	
+procedure generarVector(var v: vector; var dimL: integer);
+var
+	num: integer;
+begin
+	num := Random(100);
+	if (num <> 0) then begin
+		agregarVector(num, v, dimL);
+		generarVector(v, dimL);
+	end;
+end;
+
+procedure busquedaDicotomica(v: vector; ini, fin: indice; dato: integer; var pos: indice);
+var
+    medio: indice;
+begin
+    medio := (ini + fin) DIV 2;
+
+    while (ini <= fin) and (dato <> v[medio]) do begin
+        if (dato < v[medio]) then
+            fin := medio - 1
+        else
+            ini := medio + 1;
+        
+        medio := (ini + fin) DIV 2;
+    end;
+
+    if (ini <= fin) and (dato = v[medio]) then
+        pos := medio
+    else
+        pos := -1;
+end;
+
+var
+	v: vector;
+	dimL: integer;
+    pos: indice;
+
+begin
+	Randomize;
+	generarVector(v, dimL);
+    imprimirVector(v, dimL);
+    busquedaDicotomica(v, 1, dimL, 10, pos);
+    writeln('Posicion del 10 en el vector: ', pos);
+end.
