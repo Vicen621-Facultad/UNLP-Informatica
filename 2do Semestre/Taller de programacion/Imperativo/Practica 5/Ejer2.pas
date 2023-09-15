ba57b94e62e9a58c3b98c422a2356dbdb9dda088
Vicen621 <71749981+vicen621@users.noreply.github.com>
2023-09-15 18:34:16 -0300

    Terminado E2P5 taller

--- 2do Semestre/Taller de programacion/Imperativo/Practica 5/Ejer2.pas
+++ 2do Semestre/Taller de programacion/Imperativo/Practica 5/Ejer2.pas
@@ -1,7 +1,9 @@
 program Ejer2;
+const
+	DimF = 2018;
 
 type
-	anios = 2010..2018;
+	anios = 2010..DimF;
 	
 	auto_sm = record
 		patente: string[7];
@@ -14,15 +16,21 @@ type
 		info: auto_sm;
 	end;
 
-	lista = ^nodoL;
-	nodoL = record
+	lista_sm = ^nodo_sm;
+	nodo_sm = record
 		dato: auto_sm;
-		sig: lista;
+		sig: lista_sm;
 	end;
-	
+
+	listaA = ^nodoA;
+	nodoA = record
+		dato: auto;
+		sig: listaA;
+	end;
+
 	marca = record
 		nombre: string[25];
-		autos: lista;
+		autos: lista_sm;
 	end;
 	
 	arbolP = ^nodoP;
@@ -39,14 +47,24 @@ type
 		HD: arbolM;
 	end;
 
+	vector = array[anios] of listaA;
+
 procedure leerAuto(var a: auto);
+var
+	i: integer;
 begin
 	write('Ingrese la patente: ');
 	readln(a.info.patente);
 	if (a.info.patente <> 'XX000XX') then begin
 		write ('Ingrese la marca del auto: ');
 		readln(a.marca);
-		a.info.modelo := 'X';
+
+		// Genero un nombre de modelo al azar
+		SetLength(a.info.modelo, 6);
+		for i := 1 to 6 do
+			a.info.modelo[i] := chr(Random(123-97) + 97);
+		writeln('Modelo: ', a.info.modelo);
+
 		// Genero un numero al azar entre el 2010 y el 2018
 		a.info.anio := Random(9) + 2010;
 		writeln('Anio de fabricacion: ', a.info.anio);
@@ -68,38 +86,9 @@ begin
 			agregarArbolPatente(a^.HD, au);
 end;
 
-procedure agregarArbolMarca(var a: arbolM; m: marca);
-begin
-	if (a = nil) then begin
-		new(a);
-		a^.dato := m;
-		a^.dato.autos := nil
-		// agrego auto  la lista
-		a^.HI := nil;
-		a^.HD := nil;
-	end
-	else
-		if es igual
-			agregar a la lista
-		else if (m.nombre < a^.dato.nombre) then
-			agregarArbolMarca(a^.HI, m)
-		else
-			agregarArbolMarca(a^.HD, m);
-end;
-
-function obtenerMarca(a: arbolM; m: string): arbolM;
-begin
-	if (a = nil) or (a^.dato.nombre = m) then
-		obtenerMarca := a
-	else if (m < a^.dato.nombre) then
-		obtenerMarca(a^.HI, m)
-	else
-		obtenerMarca(a^.HD, m);
-end;
-
-procedure agregarAdelante(var l: lista; a: auto_sm);
+procedure agregarAdelante(var l: lista_sm; a: auto_sm);
 var
-	aux: lista;
+	aux: lista_sm;
 begin
 	new(aux);
 	aux^.dato := a;
@@ -107,96 +96,171 @@ begin
 	l := aux;
 end;
 
-procedure cargarMarca(var m: marca; a: auto);
+procedure agregarArbolMarca(var a: arbolM; au: auto);
 begin
-	m.nombre := a.marca;
-	m.autos := nil;
-	agregarAdelante(m.autos, a.info);
-end;
-
-procedure cargarArbolMarca(var aMarcas: arbolM; a: auto);
-var
-	aux: arbolM;
-	m: marca;
-begin
-	aux := obtenerMarca(aMarcas, a.marca);
-	if (aux = nil) then begin
-		cargarMarca(m, a);
-		agregarArbolMarca(aMarcas, m);
+	if (a = nil) then begin
+		new(a);
+		a^.dato.nombre := au.marca;
+		a^.dato.autos := nil;
+		agregarAdelante(a^.dato.autos, au.info);
+		a^.HI := nil;
+		a^.HD := nil;
 	end
 	else
-		agregarAdelante(aux^.dato.autos, a.info);
+		if (au.marca = a^.dato.nombre) then
+			agregarAdelante(a^.dato.autos, au.info)
+		else if (au.marca < a^.dato.nombre) then
+			agregarArbolMarca(a^.HI, au)
+		else
+			agregarArbolMarca(a^.HD, au);
 end;
 
 procedure cargarArboles(var aPatentes: arbolP; var aMarcas: arbolM);
 var
 	a: auto;
 begin
+	aPatentes := nil;
+	aMarcas := nil;
 	leerAuto(a);
 	while (a.info.patente <> 'XX000XX') do begin
 		agregarArbolPatente(aPatentes, a);
-		writeln('DEBUG: Agregado a patentes');
-		cargarArbolMarca(aMarcas, a);
-		writeln('DEBUG: Agregado a marcas');
+		agregarArbolMarca(aMarcas, a);
 		leerAuto(a);
 	end;
 end;
 
-procedure imprimirAuto(a: auto);
+// Le pasas dos marcas, si son iguales devuelve 1, de lo contrario devuelve 0
+function mismaMarca(nombre, actual: string): integer;
 begin
-	writeln('Marca: ', a.marca, 
-			'   Patente: ', a.info.patente, 
-			'   Modelo: ', a.info.modelo, 
-			'   Anio: ', a.info.anio);
+	if (nombre = actual) then
+		mismaMarca := 1
+	else
+		mismaMarca := 0;
 end;
 
-procedure imprimirArbolPatentes(a: arbolP);
+// B) Contar Marca Arbol Patentes
+function contarMarcasAP(aPatentes: arbolP; nombre: string): integer;
+begin
+	if (aPatentes <> nil) then begin
+		contarMarcasAP := mismaMarca(aPatentes^.dato.marca, nombre) + contarMarcasAP(aPatentes^.HI, nombre) + contarMarcasAP(aPatentes^.HD, nombre);
+	end
+	else
+		contarMarcasAP := 0;
+end;
+
+function encontrarNodo(a: arbolM; nombre: string): arbolM;
+begin
+	if (a = nil) or (a^.dato.nombre = nombre) then
+		encontrarNodo := a
+	else if (nombre < a^.dato.nombre) then
+		encontrarNodo := encontrarNodo(a^.HI, nombre)
+	else
+		encontrarNodo := encontrarNodo(a^.HD, nombre);
+end;
+
+// Cuenta la cantidad de nodos de una lista. Recursivo
+function contarNodosLista(l: lista_sm): integer;
+begin
+	if (l <> nil) then
+		contarNodosLista := 1 + contarNodosLista(l^.sig)
+	else
+		contarNodosLista := 0;
+end;
+
+// C) Contar Marca Arbol Marcas
+function contarMarcasAM(aMarcas: arbolM; nombre: string): integer;
+var
+	aux: arbolM;
+begin
+	aux := encontrarNodo(aMarcas, nombre);
+	if (aux = nil) then
+		contarMarcasAM := 0
+	else
+		contarMarcasAM := contarNodosLista(aux^.dato.autos);
+end;
+
+procedure inicializarListas(var v: vector);
+var
+	i: integer;
+begin
+	for i := 2010 to DimF do
+		v[i] := nil;
+end;
+
+procedure agregarAdelanteAutos(var l: listaA; a: auto);
+var
+	aux: listaA;
+begin
+	new(aux);
+	aux^.dato := a;
+	aux^.sig := l;
+	l := aux;
+end;
+
+// D)
+procedure cargarVector(var v: vector; a: arbolP);
 begin
 	if (a <> nil) then begin
-		imprimirArbolPatentes(a^.HI);
-		imprimirAuto(a^.dato);
-		imprimirArbolPatentes(a^.HD);
+		agregarAdelanteAutos(v[a^.dato.info.anio], a^.dato);
+		cargarVector(v, a^.HI);
+		cargarVector(v, a^.HD);
 	end;
 end;
 
-procedure imprimirAuto_sm(a: auto_sm);
+// E)
+procedure obtenerModeloAP(a: arbolP; patente: string; var ret: string);
 begin
-	writeln('Patente: ', a.patente, 
-			'   Modelo: ', a.modelo, 
-			'   Anio: ', a.anio);
+	if (a <> nil) then begin
+		if (patente = a^.dato.info.patente) then
+			ret := a^.dato.info.modelo
+		else if (patente < a^.dato.info.patente) then
+			obtenerModeloAP(a^.HI, patente, ret)
+		else
+			obtenerModeloAP(a^.HD, patente, ret);
+	end;
 end;
 
-procedure imprimirLista(l: lista);
+procedure buscarEnLista(l: lista_sm; patente: string; var ret: string);
 begin
 	while (l <> nil) do begin
-		imprimirAuto_sm(l^.dato);
-		writeln('');
-		l := l^.sig;
+		if (patente = l^.dato.patente) then begin
+			ret := l^.dato.modelo;
+			l := nil;
+		end
+		else
+			l := l^.sig;
 	end;
 end;
 
-procedure imprimirMarca(m: marca);
-begin
-	writeln('Marca: ', m.nombre, '   Autos:');
-	imprimirLista(m.autos);
-end;
-
-procedure imprimirArbolMarcas(a: arbolM);
+// F)
+procedure obtenerModeloAM(a: arbolM; patente: string; var ret: string);
 begin
 	if (a <> nil) then begin
-		imprimirArbolMarcas(a^.HI);
-		imprimirMarca(a^.dato);
-		imprimirArbolMarcas(a^.HD);
+		buscarEnLista(a^.dato.autos, patente, ret);
+		obtenerModeloAM(a^.HI, patente, ret);
+		obtenerModeloAM(a^.HD, patente, ret);
 	end;
 end;
 
 var
 	aPatentes: arbolP;
 	aMarcas: arbolM;
+	v: vector;
+	ret: string;
 begin
+	Randomize;
+
 	cargarArboles(aPatentes, aMarcas);
-	writeln('-------- Arbol de Patentes --------');
-	imprimirArbolPatentes(aPatentes);
-	writeln('-------- Arbol de Marcas --------');
-	imprimirArbolMarcas(aMarcas);
+
+	writeln('Cantidad de autos para Fiat (Patentes): ', contarMarcasAP(aPatentes, 'Fiat'));
+	writeln('Cantidad de autos para Fiat (Marcas): ', contarMarcasAM(aMarcas, 'Fiat'));
+
+	inicializarListas(v);
+	cargarVector(v, aPatentes);
+
+	obtenerModeloAP(aPatentes, 'AA000AA', ret);
+	writeln('Modelo del auto con patente AA000AA (Patentes): ', ret);
+
+	obtenerModeloAM(aMarcas, 'AA000AA', ret);
+	writeln('Modelo del auto con patente AA000AA (Marcas): ', ret);
 end.
