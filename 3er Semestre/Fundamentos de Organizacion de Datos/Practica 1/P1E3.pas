4c9d070feab449e30cd4dd268e3fde6ab9636286
Vicen621 <71749981+vicen621@users.noreply.github.com>
2024-03-20 02:25:17 -0300

    Terminado ejer 3

--- 3er Semestre/Fundamentos de Organizacion de Datos/Practica 1/P1E3.pas
+++ 3er Semestre/Fundamentos de Organizacion de Datos/Practica 1/P1E3.pas
@@ -2,8 +2,8 @@ program P1E3;
 
 type
     empleado = record
-        num, edad, dni: integer;
-        nombre, apellido: String[40];
+        num, edad: integer;
+        dni, nombre, apellido: String[40];
     end;
 
     archEmpleados = file of empleado;
@@ -11,10 +11,10 @@ type
 procedure leerEmpleado(var emp: empleado);
 begin
     write('Ingrese numero de empleado: '); ReadLn(emp.num);
-    write('Ingrese edad: '); ReadLn(emp.edad);
-    write('Ingrese DNI: '); ReadLn(emp.dni);
     write('Ingrese nombre: '); ReadLn(emp.nombre);
     write('Ingrese Apellido (fin para terminar): '); ReadLn(emp.apellido);
+    write('Ingrese edad: '); ReadLn(emp.edad);
+    write('Ingrese DNI: '); ReadLn(emp.dni);
 end;
 
 procedure imprimirEmpleado(emp: empleado);
@@ -38,6 +38,7 @@ begin
         write(arch, emp);
         leerEmpleado(emp);
     end;
+    WriteLn('--- Archivo creado exitosamente! ---');
     close(arch);
 end;
 
@@ -85,6 +86,105 @@ begin
     close(arch);
 end;
 
+//Ejercicio 4
+procedure anadirEmpleados(var arch: archEmpleados);
+var
+    emp, other: empleado;
+    exists: boolean;
+begin
+    Reset(arch);
+    leerEmpleado(emp);
+    while (emp.apellido <> 'fin') do begin
+        exists := false;
+
+        while (not eof(arch)) and (not exists) do begin
+            read(arch, other);
+            if (other.num = emp.num) then
+                exists := true
+        end;
+
+        if (exists) then
+            WriteLn('--- ERROR: Ese empleado ya existe! ---')
+        else
+            write(arch, emp);
+
+        leerEmpleado(emp);
+    end;
+    WriteLn('--- Empleados agregados con exito! ---');
+    Close(arch);
+end;
+
+procedure modificarEdad(var arch: archEmpleados);
+var 
+    emp: empleado;
+    aux: integer;
+    found: boolean;
+begin
+    Write('Ingrese el numero del empleado que desea modificar: '); ReadLn(aux);
+    
+    Reset(arch);
+    found := false;
+    
+    while(not eof(arch)) and (not found) do begin
+        read(arch, emp);
+        if (emp.num = aux) then begin
+            found := true;
+            // Vuelvo a la posicion del registro que acabo de leer
+            Seek(arch, filePos(arch) - 1);
+        end;
+    end;
+
+    if (found) then begin
+        Write('Ingrese la nueva edad del empleado: '); ReadLn(aux);
+        emp.edad := aux;
+        Write(arch, emp);
+        WriteLn('--- Edad del empleado ', emp.num, ' actualizada con exito! ---');
+    end
+    else
+        WriteLn('--- ERROR: Empleado no encontrado! ---');
+
+    Close(arch);
+end;
+
+procedure exportarAtxt(var arch: archEmpleados);
+var
+    emp: empleado;
+    txt: Text;
+begin
+    Assign(txt, 'temp/todos_empleados.txt');
+    Rewrite(txt);
+    Reset(arch);
+
+    while (not eof(arch)) do begin
+        read(arch, emp);
+        WriteLn(txt, emp.num, ' ', emp.nombre, ' ', emp.apellido, ' ', emp.edad, ' ', emp.dni);
+    end;
+    WriteLn('--- Los datos fueron exportados al archivo "todos_empleados.txt" ---');
+
+    Close(txt);
+    Close(arch);
+end;
+
+procedure exportarSinDNIAtxt(var arch: archEmpleados);
+var
+    emp: empleado;
+    txt: Text;
+begin
+    Assign(txt, 'temp/faltaDNIEmpleado.txt');
+    Rewrite(txt);
+    Reset(arch);
+
+    while (not eof(arch)) do begin
+        read(arch, emp);
+        if (emp.dni = '00') then
+            WriteLn(txt, emp.num, ' ', emp.nombre, ' ', emp.apellido, ' ', emp.edad, ' ', emp.dni);
+    end;
+    WriteLn('--- Los datos fueron exportados al archivo "faltaDNIEmpleado.txt" ---');
+    
+    Close(txt);
+    Close(arch);
+end;
+
 var
     opc: byte;
     fileName: String[40];
@@ -97,16 +197,22 @@ begin
     repeat
         WriteLn;
         WriteLn('0. Terminar el programa');
-        WriteLn('1. Crear un archivo binario de empleados');
+        WriteLn('1. Crear un archivo de empleados');
         WriteLn('2. Mostrar empleados por nombre/apellido');
         WriteLn('3. Mostrar todos los empleados');
         WriteLn('4. Mostrar en pantalla los empleados mayores de 70 años');
+        // Ejercicio 4
+        WriteLn('5. Añadir empleados a un archivo ya creado');
+        WriteLn('6. Modificar la edad de un empleado');
+        WriteLn('7. Exportar informacion');
+        WriteLn('8. Exportar empleados sin DNI');
 
         Write('Ingrese el numero de opcion: '); ReadLn(opc);
         
         if (opc <> 0) and (fileName = '') then begin
             WriteLn;
             Write('Ingrese el nombre del archivo: '); ReadLn(fileName);
+            fileName := 'temp/' + fileName;
             Assign(arch, fileName);
         end;
 
@@ -121,6 +227,11 @@ begin
             end;
             3: imprimirEmpleados(arch);
             4: imprimirEmpleadosMayor70(arch);
+            // Ejercicio 4
+            5: anadirEmpleados(arch);
+            6: modificarEdad(arch);
+            7: exportarAtxt(arch);
+            8: exportarSinDNIAtxt(arch);
         end;
     until(opc = 0);
 end.
